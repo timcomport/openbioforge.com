4dd5404c1c50e3aab5178d8527917a2698ce19da
Tim Comport <timcomport@gmail.com>
2025-07-18 15:41:44 +1000

    chore: initial commit

---
+++ js/site.js
@@ -0,0 +1,77 @@
+"use strict";
+
+$(document).ready(function () {
+	/*ScrollUp*/
+	if (!!$.prototype.scrollUp) {
+		$.scrollUp();
+	}
+
+	/*Responsive Navigation*/
+	$("#nav-mobile").html($("#nav-main").html());
+	$("#nav-trigger span").on("click",function() {
+		if ($("nav#nav-mobile ul").hasClass("expanded")) {
+			$("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+			$(this).removeClass("open");
+		} else {
+			$("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+			$(this).addClass("open");
+		}
+	});
+
+	$("#nav-mobile").html($("#nav-main").html());
+	$("#nav-mobile ul a").on("click",function() {
+		if ($("nav#nav-mobile ul").hasClass("expanded")) {
+			$("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+			$("#nav-trigger span").removeClass("open");
+		}
+	});
+
+	/* Sticky Navigation */
+	if (!!$.prototype.stickyNavbar) {
+		$('#header').stickyNavbar({
+			mobile: true
+		});
+	}
+
+	$('#content').waypoint(function (direction) {
+		if (direction === 'down') {
+			$('#header').addClass('nav-solid fadeInDown');
+		}
+		else {
+			$('#header').removeClass('nav-solid fadeInDown');
+		}
+	});
+
+});
+
+
+/* Preloader and animations */
+$(window).load(function () { // makes sure the whole site is loaded
+	$('#status').fadeOut(); // will first fade out the loading animation
+	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('body').delay(350).css({'overflow-y': 'visible'});
+
+	/* WOW Elements */
+	if (typeof WOW == 'function') {
+		new WOW().init();
+	}
+
+	/* Parallax Effects */
+	if (!!$.prototype.enllax) {
+		// Custom parallax for banner pseudo-element
+		$(window).on('scroll', function() {
+			var scrolled = $(window).scrollTop();
+			var parallaxRatio = 0.5; // Same as data-enllax-ratio
+			var translateY = scrolled * parallaxRatio;
+			
+			var banner = $('#banner');
+			if (banner.length) {
+				banner[0].style.setProperty('--parallax-offset', translateY + 'px');
+			}
+		});
+		
+		// Apply enllax to other elements (excluding banner)
+		$('[data-enllax-ratio]').not('#banner').enllax();
+	}
+
+});
